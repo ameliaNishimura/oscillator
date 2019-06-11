b78aff0b1dacae5f4a16523257d09bbb040d0912
Amelia <anishimura@wpi.edu>
2019-06-11 14:44:52 -0400

    folder add

---
+++ Oscillator/oscillator.c
@@ -0,0 +1,16 @@
+/* FEM Quantum Harmonic Oscillator
+ * Author: Amelia Nishimura
+ * 
+ * Generates approximations of the energy levels and wavefunctions of the quantum
+ * harmonic oscillator using finite element analysis (FEM).
+ */
+
+/** Main function.
+ *  @param argc Number of command line arguments.
+ *  @param argv Array of pointers to character strings containing arguments.
+ *  @return 0 if successfully run, 1 if not.
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
